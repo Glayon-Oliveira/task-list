ce7403bb4afe5866173399099705a438c8d77c2c
Glayon Oliveira <glayonsantana@gmail.com>
2025-04-18 19:07:47 -0300

    Task list

---
+++ src/main/java/com/lmlasml/tasklist/controller/SignController.java
@@ -0,0 +1,70 @@
+package com.lmlasml.tasklist.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.lmlasml.tasklist.dto.JWTTokenDTO;
+import com.lmlasml.tasklist.dto.LoginDTO;
+import com.lmlasml.tasklist.dto.SignupDTO;
+import com.lmlasml.tasklist.dto.UserDTO;
+import com.lmlasml.tasklist.model.User;
+import com.lmlasml.tasklist.service.JwtService;
+import com.lmlasml.tasklist.service.UserService;
+
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/sign")
+public class SignController {
+	
+	private UserService userService;
+	private JwtService jwtService;
+	private AuthenticationManager manager;	
+	
+	@PostMapping("/in")
+	@ResponseBody
+	@ResponseStatus(code = HttpStatus.OK)
+	public JWTTokenDTO inByJson(@RequestBody @Valid LoginDTO login) {		
+		Authentication auth = new UsernamePasswordAuthenticationToken(login.getUsername(), login.getPassword());		
+		auth = manager.authenticate(auth);
+		
+		User user = (User) auth.getDetails();		
+		String[] roles = user.getAuthorities().stream().map(GrantedAuthority::getAuthority).toArray(String[]::new);		
+		String token = jwtService.gerateToken(user.getId(), roles);		
+		return new JWTTokenDTO(token);
+	}
+	
+	@PostMapping("/in")
+	@ResponseBody
+	@ResponseStatus(code = HttpStatus.OK)
+	public JWTTokenDTO inByForm(@ModelAttribute @Valid LoginDTO login) {
+		return inByJson(login);
+	}
+	
+	@PostMapping("/up")
+	@ResponseBody
+	@ResponseStatus(code = HttpStatus.OK)
+	public UserDTO upByJson(@RequestBody @Valid SignupDTO signup) {
+		return userService.save(signup);		
+	}
+	
+	@PostMapping("/up")
+	@ResponseBody
+	@ResponseStatus(code = HttpStatus.OK)
+	public UserDTO upByForm(@ModelAttribute @Valid SignupDTO signup) {
+		return upByJson(signup);
+	}
+
+}
---
+++ src/main/java/com/lmlasml/tasklist/controller/UserController.java
@@ -0,0 +1,57 @@
+package com.lmlasml.tasklist.controller;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.lmlasml.tasklist.dto.UserDTO;
+import com.lmlasml.tasklist.security.AuthenticatedTool;
+import com.lmlasml.tasklist.service.UserService;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+	private UserService userService;	
+	
+	@PutMapping("/")
+	@ResponseBody
+	@ResponseStatus(code = HttpStatus.OK)
+	public UserDTO updatePassword(@RequestParam String password) {		
+		return userService.updatePassword(AuthenticatedTool.getUserId(), password);		
+	}
+	
+	@DeleteMapping("/")
+	@ResponseBody
+	@ResponseStatus(code = HttpStatus.OK)
+	public Void delete() {		
+		userService.delete(AuthenticatedTool.getUserId());
+		return null;
+	}
+		
+	@GetMapping("/i")
+	@ResponseBody
+	@ResponseStatus(code = HttpStatus.OK)
+	public UserDTO findByI() {				
+		return userService.findById(AuthenticatedTool.getUserId());		
+	}
+	
+	@GetMapping("/")
+	@ResponseBody
+	@ResponseStatus(code = HttpStatus.OK)	
+	public Page<UserDTO> findAll(Pageable pageable){
+		return userService.findAll(pageable);
+	}
+	
+}
