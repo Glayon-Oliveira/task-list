7857b431b149c98ac28fb87878a7f224e450be3f
Glayon Oliveira <glayonsantana@gmail.com>
2025-04-21 20:14:59 -0300

    chore: project adjustments and added Docker support

--- src/main/java/com/lmlasml/tasklist/security/SecurityConf.java
+++ src/main/java/com/lmlasml/tasklist/security/SecurityConf.java
@@ -13,7 +13,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
-import com.lmlasml.tasklist.repository.JwtAuthenticationFilter;
+import com.lmlasml.tasklist.filter.JwtAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
