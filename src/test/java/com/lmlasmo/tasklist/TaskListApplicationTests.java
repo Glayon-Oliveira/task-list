1a72cce904baded66fac38cb5e81912ad83d1614
Glayon Oliveira <glayonsantana@gmail.com>
2025-11-04 21:03:50 -0300

    Add EmailService e properties

--- src/test/java/com/lmlasmo/tasklist/TaskListApplicationTests.java
+++ src/test/java/com/lmlasmo/tasklist/TaskListApplicationTests.java
@@ -2,9 +2,15 @@ package com.lmlasmo.tasklist;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+
+import com.lmlasmo.tasklist.service.EmailService;
 
 @SpringBootTest
 public class TaskListApplicationTests {
+	
+	@MockitoBean
+	private EmailService emailService;
 
 	@Test
 	public void contextLoads() {}
